c823b04f459330d026a0fa6a830a4377d0378155
Justin Bayer <bayerj@argmax.ai>
2022-11-15 13:30:59 +0100

    change a few parameters

--- run.py
+++ run.py
@@ -6,17 +6,17 @@ import matplotlib.pyplot as plt
 import scipy.stats as scst
 import seaborn as sns
 
-rng = np.random.RandomState(42)
+rng = np.random.RandomState(45)
 
 
 def sample_states(
     n_time_steps: int,
     initial_state_dist: np.ndarray,
-    transition_matrix: np.ndarray,
+    transition: np.ndarray,
 ) -> np.ndarray:
     states = [rng.multinomial(1, initial_state_dist)]
     for i in range(n_time_steps - 1):
-        dist = transition_matrix @ states[-1]
+        dist = transition @ states[-1]
         states.append(rng.multinomial(1, dist))
 
     return np.stack(states, 0)
@@ -32,10 +32,10 @@ def sample_emisson(
 def sample(
     n_time_steps: int,
     initial_state_dist: np.ndarray,
-    transition_matrix: np.ndarray,
+    transition: np.ndarray,
     obs_scales: np.ndarray,
 ) -> typing.Tuple[np.ndarray, np.ndarray]:
-    states = sample_states(n_time_steps, initial_state_dist, transition_matrix)
+    states = sample_states(n_time_steps, initial_state_dist, transition)
     emissions = sample_emisson(states, obs_scales)
     return states, emissions
 
@@ -48,7 +48,7 @@ def emit_log_probs(obs: np.ndarray, obs_scales: np.ndarray):
 
 def infer_states(
     initial_state_dist: np.ndarray,
-    transition_matrix: np.ndarray,
+    transition: np.ndarray,
     obs_scales: np.ndarray,
     obs: np.ndarray,
 ) -> np.ndarray:
@@ -62,7 +62,7 @@ def infer_states(
     filtrs = [filtr]
 
     for i in range(1, n_time_steps):
-        prior = transition_matrix @ filtrs[-1]
+        prior = transition @ filtrs[-1]
         log_priors.append(np.log(prior + 1e-8))
         unn_log_filter = log_priors[-1] + emit_log_probs(
             obs=obs[i], obs_scales=obs_scales
@@ -74,34 +74,20 @@ def infer_states(
     return np.stack(filtrs, 0)
 
 
-if __name__ == "__main__":
-    n_time_steps = 255
-    initial_state_dist = np.array([0.5, 0.5])
-    transition_matrix = np.array([[0.9, 0.1], [0.1, 0.9]])
-    obs_scales = np.array([0.2, 1.0])
+def plot(states, observations, inferred):
+    fig, axs = plt.subplots(3, 1, figsize=(12, 6), sharex=True)
 
-    states = sample_states(n_time_steps, initial_state_dist, transition_matrix)
-    emissions = sample_emisson(states, obs_scales=obs_scales)
-    inferred = infer_states(
-        obs=emissions,
-        initial_state_dist=initial_state_dist,
-        obs_scales=obs_scales,
-        transition_matrix=transition_matrix,
-    )
-
-    fig, axs = plt.subplots(3, 1, figsize=(6, 6))
-
-    axs[0].imshow(states.T, cmap="binary")
+    axs[0].imshow(states.T, cmap="binary", interpolation="none")
     axs[0].set_aspect("auto")
     axs[0].set_title("states")
     axs[0].set_yticks([])
     axs[0].set_xticks([])
 
     axs[1].set_title("observations")
-    axs[1].plot(emissions)
+    axs[1].plot(observations)
     axs[0].set_xticks([])
 
-    axs[2].imshow(inferred.T, cmap='binary')
+    axs[2].imshow(inferred.T, cmap="binary", interpolation="none")
     axs[2].set_aspect("auto")
     axs[2].set_title("inferred states")
     axs[2].set_yticks([])
@@ -109,4 +95,28 @@ if __name__ == "__main__":
     plt.tight_layout()
     sns.despine(fig)
 
+    plt.savefig("output.png")
     plt.show()
+
+
+if __name__ == "__main__":
+    n_time_steps = 250
+    initial_state_dist = np.array([0.99, 0.01])
+    transition= np.array(
+        [
+            [0.975, 1e-8], 
+            [0.025, 1. - 1e-8]
+        ]
+    )
+    obs_scales = np.array([0.03, 1.0])
+
+    states = sample_states(n_time_steps, initial_state_dist, transition)
+    emissions = sample_emisson(states, obs_scales=obs_scales)
+    inferred = infer_states(
+        obs=emissions,
+        initial_state_dist=initial_state_dist,
+        obs_scales=obs_scales,
+        transition=transition,
+    )
+
+    plot(states=states, observations=emissions, inferred=inferred)
